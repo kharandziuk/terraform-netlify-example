8aed8e06ff4da4cffdc52c5c02d2d3968d8e88d3
Max Kharandziuk <max.kharandziuk@gmail.com>
2020-06-07 10:55:35 +0200

    proper name for a variable

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to {process.env.NAME}.
+          Edit <code>src/App.js</code> and save to {process.env.REACT_APP_NAME}.
         </p>
         <a
           className="App-link"
